77f74edbc3b468d581ad15b569a529a441684788
alvaro bianor <bianor.alvaro12@gmail.com>
2021-07-25 15:45:01 -0300

    Adding Types

--- src/routes/systemRoutes/todo.route.ts
+++ src/routes/systemRoutes/todo.route.ts
@@ -5,6 +5,18 @@ import TodoModel from '../../models/Todo';
 const usersRouter = Router();
 
 // ROUTES
+interface PropsTodo {
+	_id: string;
+	name: string;
+	data: string;
+	description: string;
+	status: ENUMStatus;
+	createdAt: Date;
+	updatedAt: Date;
+	save: () => void;
+}
+type CreateTodo = Omit<PropsTodo, 'createdAt' | 'updatedAt'>;
+type ENUMStatus = 'CONCLUIDO' | 'PENDENTE' | 'CANCELADO';
 
 usersRouter.post('/', async (req, res) => {
 	const { name, description, date } = req.body;
@@ -15,19 +27,19 @@ usersRouter.post('/', async (req, res) => {
 
 	const [day, mounth, year] = date.split('/');
 
-	const todo = new TodoModel({
+	const todo: CreateTodo = new TodoModel({
 		name: name,
 		date: `${day}/${mounth}/${year}`,
 		description: description,
 		status: 'CONCLUIDA',
 	});
 	await todo.save();
-	return res.status(201).json({ TODO: todo });
+	return res.status(201).json({ message: 'Success!' });
 });
 
 usersRouter.get('/all', async (req, res) => {
 	const { filter } = req.query;
-	let all = {};
+	let all: PropsTodo = {} as unknown as PropsTodo;
 	if (
 		filter === 'PENDENTE' ||
 		filter === 'CONCLUIDA' ||
@@ -48,7 +60,7 @@ usersRouter.put('/changeStatus', async (req, res) => {
 		return res.status(400).json({ message: 'Something is broken' });
 	}
 
-	const todo = await TodoModel.findOne({ _id: id });
+	const todo: PropsTodo = await TodoModel.findOne({ _id: id });
 
 	todo.status = status;
 
