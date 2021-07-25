c518fc99db44d2d359cb3f616b305f9ec87957f1
alvaro bianor <bianor.alvaro12@gmail.com>
2021-07-25 11:22:31 -0300

    Delete log

--- src/server.ts
+++ src/server.ts
@@ -11,4 +11,4 @@ app.use(express.json());
 // app.use(cors);
 app.use(routes);
 
-app.listen(3333, () => console.log('Entrou', process.env.PASSWORD));
+app.listen(3333, () => console.log('Entrou'));
